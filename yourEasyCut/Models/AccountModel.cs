0e200b7445b1da7cd030c1de84d6c230343a92aa
Antoine Jacob-Tremblay <antoinejt@gmail.com>
2012-06-23 13:54:52 -0400

    test de css

--- yourEasyCut/Models/AccountModel.cs
+++ yourEasyCut/Models/AccountModel.cs
@@ -10,12 +10,12 @@ namespace yourEasyCut.Models
     public class LogOnModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "User name: ")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Password: ")]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
